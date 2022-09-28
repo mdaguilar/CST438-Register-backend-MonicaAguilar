ca2ecfaeffb8cdeb9b9fe6a489d5d8a00f286320
Monica A. <m.d.aguilar@outlook.com>
2022-09-27 23:43:49 -0700

    updated StudentController

--- cst438_register/src/main/java/com/cst438/controller/StudentController.java
+++ cst438_register/src/main/java/com/cst438/controller/StudentController.java
@@ -1,10 +1,15 @@
 package com.cst438.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 	
+	@PostMapping("/student")
+	public String createNewStudent() {
+		return "student ID = 12345";
+	}
 	
 
 }
